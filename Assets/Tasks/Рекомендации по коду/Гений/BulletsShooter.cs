88dc398760dbf36c8075fac0f4fed1cf2eaf1cd6
st-lnad <lev.potanin@yandex.ru>
2023-12-13 14:13:59 +0300

    Update Task Genious

--- Assets/Tasks/Рекомендации по коду/Гений/BulletsShooter.cs
+++ Assets/Tasks/Рекомендации по коду/Гений/BulletsShooter.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class BulletsShooter : MonoBehaviour
 {
     [SerializeField] private float _bulletSpeed;
     [SerializeField] private Rigidbody _bulletPrefab;
     [SerializeField] private float _cooldown;
-    public Transform Target;
+    [SerializeField] private Transform _target;
 
     private void Start()
     {
         StartCoroutine(_shootingWorker());
     }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+
     private IEnumerator _shootingWorker()
     {
         bool isWork = enabled;
         var delay = new WaitForSeconds(_cooldown);
         while (isWork)
         {
-            Vector3 _direction = (Target.position - transform.position).normalized;
-            Rigidbody NewBullet = Instantiate(_bulletPrefab, transform.position + _direction, Quaternion.identity);
+            Vector3 _direction = (_target.position - transform.position).normalized;
+            Rigidbody newBullet = Instantiate(_bulletPrefab, transform.position + _direction, Quaternion.identity);
 
-            NewBullet.transform.up = _direction;
-            NewBullet.velocity = _direction * _bulletSpeed;
+            newBullet.transform.up = _direction;
+            newBullet.velocity = _direction * _bulletSpeed;
 
             yield return delay;
         }
--- Assets/Tasks/Рекомендации по коду/Гений/WaypointMovement.cs
+++ Assets/Tasks/Рекомендации по коду/Гений/WaypointMovement.cs
@@ -3,32 +3,35 @@ using UnityEngine;
 public class WaypointMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Transform WaypointRoot;
+    [SerializeField] private Transform _waypointRoot;
     private Transform[] _waypoints;
     private int _currentWaypointIndex;
+    private Vector3 _currentWaypointPosition;
+
     private void Start()
     { 
-        _waypoints = new Transform[WaypointRoot.childCount];
+        _waypoints = new Transform[_waypointRoot.childCount];
 
-        for (int i = 0; i < WaypointRoot.childCount; i++)
-            _waypoints[i] = WaypointRoot.GetChild(i).transform;
+        for (int i = 0; i < _waypointRoot.childCount; i++)
+            _waypoints[i] = _waypointRoot.GetChild(i);
     }
+
     private void Update()
     {
-        var _pointByNumberInArray = _waypoints[_currentWaypointIndex];
-        transform.position = Vector3.MoveTowards(transform.position, _pointByNumberInArray.position, _speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _currentWaypointPosition, _speed * Time.deltaTime);
 
-        if (transform.position == _pointByNumberInArray.position) GetNextWaypoint();
+        if (transform.position == _currentWaypointPosition) 
+            GetNextWaypoint();
     }
-    private Vector3 GetNextWaypoint()
+
+    private void GetNextWaypoint()
     {
         _currentWaypointIndex++;
 
         if (_currentWaypointIndex == _waypoints.Length)
             _currentWaypointIndex = 0;
 
-        var currentWaypointPosition = _waypoints[_currentWaypointIndex].transform.position;
-        transform.forward = currentWaypointPosition - transform.position;
-        return currentWaypointPosition;
+        _currentWaypointPosition = _waypoints[_currentWaypointIndex].transform.position;
+        transform.forward = _currentWaypointPosition - transform.position;
     }
 }
